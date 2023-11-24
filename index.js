d0fb8d6bc3f3575f5f6cc73d5178f5452db4fb7e
Si Mohamed Rayene <raykib2000@live.fr>
2023-11-24 19:26:30 +0100

    test ip thing

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ var app = express();
 app.use(bodyParser());
 app.use(cors());
 
+const Ip = mongoose.model("ip", {
+	Ip: String,
+	date: String,
+});
+
 const IoTData = mongoose.model("data", {
 	docId: Number,
 	time: String,
@@ -21,6 +26,11 @@ const IoTData = mongoose.model("data", {
 	image: String,
 	tds: String,
 	conductivity: String,
+});
+
+app.post("/chester", async (req, resp) => {
+	console.log(req.body);
+	resp.status(200).send("hello");
 });
 
 app.post("/log", async (req, resp) => {
@@ -106,11 +116,61 @@ app.get("/getdata", async (req, resp) => {
 	}
 });
 
+app.get("/piw", (req, res) => {
+	let date = new Date();
+	let day = String(date.getDate()).padStart(2, "0");
+	let month = String(date.getMonth() + 1).padStart(2, "0");
+	let year = String(date.getFullYear());
+	let hours = String(date.getHours()).padStart(2, "0");
+	let minutes = String(date.getMinutes()).padStart(2, "0");
+	let seconds = String(date.getSeconds()).padStart(2, "0");
+
+	let time = `${day}/${month}/${year} ${hours}:${minutes}:${seconds}`;
+
+	try {
+		const ip =
+			req.headers["x-forwarded-for"] ||
+			req.connection.remoteAddress ||
+			req.socket.remoteAddress ||
+			req.connection.socket.remoteAddress;
+
+		const doc = new Ip({
+			Ip: ip,
+			date: time,
+		});
+		doc
+			.save()
+			.then(() => {
+				res.status(200).json({ status: "OK" });
+			})
+			.catch((err) => {
+				res.status(500).json({ status: "Not OK" });
+				console.log(err);
+			});
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ error: "Internal Server Error" });
+	}
+});
+
 //My online testing get Api : https://vercel-test-gules-five.vercel.app/testies
 
 let check = false;
 
 app.get("/testies", (req, res) => {
+	if (check) {
+		check = false;
+		res.status(200);
+		res.json([{ piw: "piw" }, { piw: "piw" }]);
+	} else {
+		check = true;
+		res.status(200);
+		res.json([{ piw: "diw" }, { piw: "diw" }]);
+	}
+});
+
+app.post("/predict", (req, res) => {
+	console.log(req.body);
 	if (check) {
 		check = false;
 		res.status(200);
